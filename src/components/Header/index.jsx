af94cceae2a962136fc7e30d95e00559d1f7f9bb
Shiraz-Malik12 <shirazmalik1241@gmail.com>
2023-10-17 17:24:40 +0500

    header done

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+
+const Header = () => {
+  return (
+    <div>
+          <Navbar expand="lg" className="bg-body-tertiary">
+      <div className='container'>
+        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ms-auto gap-4">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#about">About</Nav.Link>
+            <Nav.Link href="#services">Services</Nav.Link>
+            <Nav.Link href="#resume">Resume</Nav.Link>
+            <Nav.Link href="#portfolio">Portfolio</Nav.Link>
+            <Nav.Link href="#contact">Contact</Nav.Link>
+        
+          </Nav>
+        </Navbar.Collapse>
+      </div>
+    </Navbar>
+
+    </div>
+  )
+}
+
+export default Header
